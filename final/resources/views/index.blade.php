f34d9b52feaad24de068f912fddecf4ccc44277f
JaviGarcia <55480199+javi097@users.noreply.github.com>
2020-02-28 17:31:22 +0100

    MODELOS -MCSR

---
+++ final/resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Articulos</title>
+</head>
+<body style="background: #0bd7b2">
+    <h3 class="text-center mt-3">ARTICULOS</h3>
+    <div class="container mt-3 text-center">
+    <a href="{{route('articulos.index')}}" class="btn btn-info mr-1">Articulos</a>
+    </div>
+</body>
+</html>
